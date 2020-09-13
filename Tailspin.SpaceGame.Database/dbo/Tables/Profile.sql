f16274b4016eb460bf2372a38e7466456421d41f
cwcordell <cwcordell@yahoo.com>
2020-09-13 15:13:43 -0500

    removed update data

--- Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
+++ Tailspin.SpaceGame.Database/dbo/Tables/Profile.sql
@@ -5,16 +5,3 @@
     [favoriteMap]       NVARCHAR(50) NULL,
     PRIMARY KEY CLUSTERED ([id] ASC)
 );
-GO
-UPDATE DBO.Profiles
-SET favoriteMap='Milky Way'
-WHERE CAST((id) AS int) % 3 = 0
-GO
-UPDATE DBO.Profiles
-SET favoriteMap='Andromeda'
-WHERE CAST((id) AS int) % 3 = 1
-GO
-UPDATE DBO.Profiles
-SET favoriteMap='Pinwheel'
-WHERE CAST((id) AS int) % 3 = 2
-GO
